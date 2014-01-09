67c473de31c4fb36e88a5a54c9ce02168cb3e0c3
Jimmy Wärting <jimmy@warting.se>
2014-01-09 22:53:52 +0100

    added deploy script

---
+++ deploy.sh
@@ -0,0 +1,34 @@
+#/bin/sh
+
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied, please enter commit message by typing deploy.sh \"message\""
+    exit 0
+fi
+
+COMMIT_MESSAGE=$1
+
+if [ -d ../css-sprite-generator-release/ ]; then
+  rm -Rf ../css-sprite-generator-release/
+fi
+
+git clone https://github.com/warting/css-sprite-generator.git ../css-sprite-generator-release
+cd ../css-sprite-generator-release/
+git checkout gh-pages
+
+for entry in "."/*
+do
+  rm -Rf $entry
+done
+
+cp -Rf ../css-sprite-generator/release/* ./
+
+git add -A
+
+git commit -am "$COMMIT_MESSAGE"
+
+git push
+
+cd ../css-sprite-generator/
+
+rm -Rf ../css-sprite-generator-release
