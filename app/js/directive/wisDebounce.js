53b9cf1cb4dffab6dac648b262d25c38e923828f
Jimmy Wärting <jimmy@warting.se>
2014-01-14 22:36:39 +0100

    debounce the saving to localStorage on every input event

---
+++ app/js/directive/wisDebounce.js
@@ -0,0 +1,28 @@
+app.directive('wisDebounce', function($timeout) {
+	return {
+		restrict: 'A',
+		require: 'ngModel',
+		priority: 99,
+		link: function(scope, elm, attr, ngModelCtrl) {
+			if (attr.type === 'radio' || attr.type === 'checkbox') return;
+
+			elm.unbind('input');
+
+			var debounce;
+			elm.bind('input', function() {
+				$timeout.cancel(debounce);
+				debounce = $timeout( function() {
+					scope.$apply(function() {
+						ngModelCtrl.$setViewValue(elm.val());
+					});
+				}, 150);
+			});
+			elm.bind('blur', function() {
+				scope.$apply(function() {
+					ngModelCtrl.$setViewValue(elm.val());
+				});
+			});
+		}
+
+	}
+});
